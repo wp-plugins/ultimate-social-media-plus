024728da35d598bcda5c99a6431816e9fc855999
socialdude <socialdude@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-15 04:41:53 +0000

    new version
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-social-media-plus/tags/1.1@1160909 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ libs/controllers/sfsiocns_OnPosts.php
@@ -0,0 +1,216 @@
+<?php
+/* add fb like add this and google share to end of every post */
+
+function sfsi_plus_social_buttons_below($content) {
+	global $post;
+         $sfsi_section6=  unserialize(get_option('sfsi_plus_section6_options',false));
+		 
+		 //new options that are added on the third questions
+		 //so in this function we are replacing all the past options 
+		 //that were saved under option6 by new settings saved under option8 
+		 $sfsi_section8=  unserialize(get_option('sfsi_plus_section8_options',false));
+		 $sfsi_plus_show_item_onposts = $sfsi_section8['sfsi_plus_show_item_onposts'];
+		 //new options that are added on the third questions
+        
+  /* check if option activated in admin or not */ 
+  //if($sfsi_section6['sfsi_plus_show_Onposts']=="yes")
+  //removing following condition for now
+  /*if($sfsi_section8['sfsi_plus_show_Onposts']=="yes")
+  {*/
+	$permalink = get_permalink($post->ID);
+        $title = get_the_title();
+	$sfsiLikeWith="45px;";
+        /* check for counter display */
+        //if($sfsi_section6['sfsi_plus_icons_DisplayCounts']=="yes")
+		if($sfsi_section8['sfsi_plus_icons_DisplayCounts']=="yes")
+		{
+            $show_count=1;
+	    	$sfsiLikeWith="75px;";
+        }   
+        else
+        {
+            $show_count=0;
+        } 
+        //$txt=(isset($sfsi_section6['sfsi_plus_textBefor_icons']))? $sfsi_section6['sfsi_plus_textBefor_icons'] : "Share this Post with :" ;
+        $txt=(isset($sfsi_section8['sfsi_plus_textBefor_icons']))? $sfsi_section8['sfsi_plus_textBefor_icons'] : "Please follow and like us:" ;
+		//$float= $sfsi_section6['sfsi_plus_icons_alignment'];
+        $float= $sfsi_section8['sfsi_plus_icons_alignment'];
+		$icons="<div class='sfsi_Sicons' style='float:".$float."'><div style='float:left;margin:0 px; line-height: 32px'><span>".$txt."</span></div>";
+        
+        $icons.="<div class='sf_fb' style='float:left;margin:5px;width:".$sfsiLikeWith."'>".sfsi_plus_FBlike($permalink,$show_count)."</div>";
+		$icons.="<div class='sf_google'  style='float:left;margin:5px;max-width:62px;min-width:35px;'>".sfsi_plus_googlePlus($permalink,$show_count)."</div>";
+        $icons.="<div class='sf_addthis'  style='float:left;margin:8px 5px 5px 5px;'>".sfsi_plus_Addthis($show_count)."</div>";
+      
+	$icons.="</div>";
+    if(!is_feed() && !is_home() && !is_page()) {
+		$content =   $content .$icons;
+	}
+  //}   
+	return $content;
+}
+
+/* create google+ button */
+function sfsi_plus_googlePlus($permalink,$show_count) {
+        $google_html = '<div class="g-plusone" data-href="' . $permalink . '" ';
+        if($show_count) {
+                $google_html .= 'data-size="large" ';
+        } else {
+                $google_html .= 'data-size="large" data-annotation="none" ';
+        }
+        $google_html .= '></div>';
+        return $google_html;
+}
+
+/* create fb like button */
+function sfsi_plus_FBlike($permalink,$show_count) {
+      
+                $send = 'false';
+                $width = 180;
+
+        $fb_like_html = '<fb:like href="'.$permalink.'" width="'.$width.'" send="'.$send.'" showfaces="false" ';
+        if($show_count==1) { 
+                $fb_like_html .= 'layout="button_count"';
+        } else {
+                $fb_like_html .= 'layout="button"';
+        }
+        $fb_like_html .= ' action="like"></fb:like>';
+        return $fb_like_html;
+}
+/* create add this  button */
+function sfsi_plus_Addthis($show_count)
+{
+   
+   $atiocn=' <script type="text/javascript">
+var addthis_config = {
+     pubid: "YOUR-PROFILE-ID"
+}
+</script>';
+
+if($show_count==1)
+   {
+       $atiocn.=' <div class="addthis_toolbox">
+              <a class="addthis_counter addthis_pill_style share_showhide"></a>
+	   </div>';
+	    return $atiocn;
+	
+   }
+   else
+   {
+	$atiocn.='<div class="addthis_toolbox addthis_default_style addthis_20x20_style"><a class="addthis_button_compact " href="#">  <img src="'.SFSI_PLUS_PLUGURL.'images/sharebtn.png"  border="0" alt="Share" /></a></div>';
+      return $atiocn; 
+    }
+}
+
+function sfsi_plus_Addthis_blogpost($show_count, $permalink, $post_title)
+{ 
+   $atiocn=' <script type="text/javascript">
+var addthis_config = {
+     url: "'.$permalink.'",
+   	 title: "'.$post_title.'"
+}
+</script>';
+if($show_count==1)
+   {
+	   $atiocn.=' <div class="addthis_toolbox" addthis:url="'.$permalink.'" addthis:title="'.$post_title.'">
+              <a class="addthis_counter addthis_pill_style share_showhide"></a>
+	   </div>';
+	    return $atiocn;
+   }
+   else
+   {
+	$atiocn.='<div class="addthis_toolbox addthis_default_style addthis_20x20_style" addthis:url="'.$permalink.'" addthis:title="'.$post_title.'"><a class="addthis_button_compact " href="#">  <img src="'.SFSI_PLUS_PLUGURL.'images/sharebtn.png"  border="0" alt="Share" /></a></div>';
+      return $atiocn; 
+    }
+}
+	
+/* add all external javascript to wp_footer */        
+ function sfsi_plus_footer_script() {
+	  $sfsi_section1=  unserialize(get_option('sfsi_plus_section1_options',false));
+	  $sfsi_section6=  unserialize(get_option('sfsi_plus_section6_options',false));
+	if($sfsi_section1['sfsi_plus_facebook_display']=="yes")
+	{
+	?>
+	<!--facebook like and share js -->                   
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=1425108201100352&version=v2.0";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+ <?php } if($sfsi_section1['sfsi_plus_google_display']=="yes" || $sfsi_section1['sfsi_plus_youtube_display']=="yes") { ?>
+ <!--google share and  like and e js -->
+	<script type="text/javascript">
+		window.___gcfg = {
+		  lang: 'en-US'
+		};
+		(function() {
+			var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+			po.src = 'https://apis.google.com/js/plusone.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+		})();
+	</script>
+	
+	<script type='text/javascript' src='https://apis.google.com/js/plusone.js'></script>
+	<script type='text/javascript' src='https://apis.google.com/js/platform.js'></script>
+	<!-- google share -->
+	<script type="text/javascript">
+	  (function() {
+	    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+	    po.src = 'https://apis.google.com/js/platform.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+	  })();
+	</script>
+<?php } if($sfsi_section1['sfsi_plus_linkedin_display']=="yes") { ?>	
+       <!-- linkedIn share and  follow js -->
+        <script src="//platform.linkedin.com/in.js" type="text/javascript">lang: en_US</script>
+	
+<?php } if($sfsi_section1['sfsi_plus_share_display']=="yes" || $sfsi_section6['sfsi_plus_show_Onposts']=="yes") { ?>		
+	 <!-- Addthis js -->
+        <script type="text/javascript" src="https://s7.addthis.com/js/300/addthis_widget.js"></script>
+        <script type="text/javascript">
+       var addthis_config = {  ui_click: true  };
+       </script>
+<?php } if($sfsi_section1['sfsi_plus_pinterest_display']=="yes") {?>
+	
+	<!--pinit js -->
+	<script type="text/javascript" src="//assets.pinterest.com/js/pinit.js"></script>
+	
+	<?php } if($sfsi_section1['sfsi_plus_twitter_display']=="yes") {?>
+<!-- twitter JS End -->
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>	
+	<?php }
+	/* activate footer credit link */
+        if(get_option('sfsi_plus_footer_sec')=="yes")
+        {
+	    if(!is_admin())
+	    {
+            //$footer_link='<div class="sfsiplus_footerLnk" style="margin: 0 auto;z-index:1000; absolute; text-align: center;">Social media & sharing icons powered by  <a href="https://wordpress.org/plugins/ultimate-social-media-icons/" target="_new">UltimatelySocial</a> ';
+			$footer_link='<div class="sfsiplus_footerLnk" style="margin: 0 auto;z-index:1000; absolute; text-align: center;">Social media & sharing icons powered by  <a href="http://ultimatelysocial.com/" target="_new">UltimatelySocial</a> ';
+	    	$footer_link.="</div>";
+	    echo $footer_link;
+	    }
+	}    
+        
+}
+/* filter the content of post */
+//commenting following code as we are going to extend this functionality 
+//add_filter('the_content', 'sfsi_plus_social_buttons_below');
+
+/* update footer for frontend and admin both */ 
+if(!is_admin())
+{   global $post;
+   add_action( 'wp_footer', 'sfsi_plus_footer_script' );	
+   add_action('wp_footer','sfsi_plus_check_PopUp');
+   add_action('wp_footer','sfsi_plus_frontFloter');	 	     
+}
+			 				    
+if(is_admin())
+{
+	add_action('in_admin_footer', 'sfsi_plus_footer_script');	
+}
+
+/* ping to vendor site on updation of new post */
+
+?>
---
+++ readme.txt
@@ -0,0 +1,135 @@
+=== Ultimate Social Media Icons PLUS ===
+Contributors: socialdude
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=seb.richter%40gmx%2enet&lc=LI
+Tags: social media, social media icons, sidebar, admin, icons, sharing icons, social media widget, social icon widget, socialmedia, fb widget, facebook, facebook icon, facebook button, facebook share button, facebook like button, twitter, twitter icon, twitter button, google+, google, google plus, google plus icon, google plus button, pinterest, pinterest icon, pinterest button, instagram, Instagram icon, Instagram button, youtube, youtube icon, youtube button, linkedin, linkedin icon, linkedin button, subscription popup, pop-up, ultimate, email, rss, rss2email, subscription icons, subscription icon, social media pop-up, free, newsletter, email newsletter, follow, follow me, counter, feed, feeds, filter, links, link, media, plugin, social, widget, tweet button, email button, email icon, socialize, facebook like, facebook recommend
+Requires at least: 3.0
+Tested up to: 4.2
+Stable tag: 1.0
+License: GPLv2 
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Super-easy to use and 100% FREE social media plugin which adds social media icons to your website with tons of customization features! 
+
+== Description ==
+
+This plugin is based on https://wordpress.org/plugins/ultimate-social-media-icons/, giving it even more functions and making it even easier to use at the same time.  
+
+It's still 100% FREE and allows you to display social media icons on your website and tailor them to your needs. 
+
+You can add icons for RSS, Email, Facebook, Twitter, LinkedIn, Google+, Pinterest, Instagram, Youtube, "Share" (covering 200+ other social media platforms) and upload custom icons of your choice. 
+
+Additional features in this plugin: 
+
+- You can place icons also before posts 
+- You can place the icons next to your posts also on your homepage
+- You can also choose to display the icons you picked before/after posts (not just a standard set)
+- Placing the icons is easier than before (new third question) 
+- Several bugs fixed
+
+As with its predecessor, you can: 
+
+- Pick from 16 different designs for your icons
+- Give several actions to one icon (e.g. your facebook icon can lead visitors to your Facebook page, and also give visitors the opportunity to like your page)
+- Decide to give your icons an animation (e.g. automatic shuffling, mouse-over effects) to make your visitors aware of them, increasing the chance that they follow/share your blog
+- Allow visitors to subscribe to your blog by Email 
+- Add "counts" to your icons
+- Decide to display a pop-up (on all or only on selected pages) asking people to follow/share you
+- Select from many other customization features
+
+The plugin is very easy to use as it takes you through the process step by step. Check out the screenshots. 
+
+
+== Installation ==
+Extract the zip file and drop the contents into the wp-content/plugins/ directory of your WordPress installation. Then activate the plugin from the plugins page.
+
+Then go to plugin settings page and answer the first 3 questions. That's it. 
+
+Note: This plugin requires CURL to be activated/installed on your server (which should be the standard case). If you don't have it, please contact your hosting provider.
+
+== Frequently Asked Questions ==
+
+= Some of the links I entered don't work =
+
+Please don't forget to enter the "http://" at the beginning of the link. 
+
+= How can I change the "Please follow & like us :)" if I placed the icons via a widget? =
+
+You can change that in the Widget-area where you dropped the widget on the sidebar. Please click on it (on the sidebar), it will open the menu where you can change the text. 
+
+For you visual people: http://screencast.com/t/euuqTzbJ 
+
+If you don't want to show any text, just enter a space (" ").
+
+= How can I remove the credit-link ("Powered by Ultimatelysocial")? =
+
+Open the first question in the plugin ("1. Where do you want to display the icons?"), on the level of the email-icon you see a link on the right hand side. Please click it to remove the credit link. 
+
+Please note that we didn't place the credit link without your consent (you agreed to it when de-selecting the email-icon). 
+
+In video: http://screencast.com/t/o0sfAgyqjWBH
+
+= Can I also display the icons vertically (on top of each other)? =
+
+Yes. For that please go to question 5 and select to display only 1 icon per row. 
+
+= I have difficulties uploading custom icons - why is that? = 
+
+Most likely that's because you've set "allow_url_fopen" to "off". Please turn it to "on" (or ask your server admin to do so - he'll know what to do). 
+
+= My Youtube icon doesn't work = 
+
+Please ensure that you've selected the radio button "Username" when you enter a youtube username, or "Channel ID" when you entered a channel ID. 
+
+= Aligning the icons (centered, left- or right-aligned) doesn't work = 
+
+The alignment options under question 6 align the icons with respect to each other (see http://screencast.com/t/VPkmjs3oV ), not where they appear on the page. 
+
+To make the icons show up at the right spot please play around with all the different placement options the plugin offers (under question 3). If that doesn't work please ask the creator of your template for advice.
+
+= This is a great plugin! How can I say "Thank you"? = 
+
+By giving us a 5-star rating :) You can give your "Thank you" even more strength with a donation - check the "Donate to this plugin" on the right hand side)
+
+= Are you guys open for taking on client work? = 
+
+Yes, absolutely. We build websites (incl. design), web applications and software, plugins :) and do theme customizations. Get in touch with us at support at ultimatelysocial dot com. 
+
+== Screenshots ==
+
+1. After installing the plugin, you'll see this overview. You'll be taken through the easy-to-understand steps to configure your plugin 
+
+2. As a first step you select which icons you want to display on your website
+
+3. Then you'll define what the icons should do (they can perform several actions, e.g. lead users to your facebook page, or allow them to share your content on their facebook page)
+
+4. In a third step you decide where the icons should be placed: a.) via Widget, b.) Floating, c.) via Shortcode and/or d.) Before or after posts
+
+5. You can pick from a wide range of icon designs
+
+6. Here you can animate your main icons (automatic shuffling, mouse-over effects etc.), to make visitors of your site aware that they can share, follow & like your site
+
+7. You can choose to display counts next to your icons (e.g. number of Twitter-followers) 
+
+8. There are many more options to choose from 
+
+9. You can also display a pop-up (designed to your liking) which asks users to like & share your site
+
+
+== Changelog ==
+
+= 1.1 =
+* Og-issues fixed
+* Conflicts with Yoast SEO plugin sorted
+* Alignments under posts didn't work sometimes before, fixed now
+* When user selected icons to shuffle pop-up didn't show up, fixed now
+* Short code corrected
+* On some templates the checkboxes in the admin area couldn't get selected, fixed now
+* Links now to the correct review screen
+* Share-box only displayed partly sometimes, fixed now
+* When sharing from a Facebook business page it returned errors, this should be fixed now (to be observed) 
+* Sometimes facebook share count didn't increase despite liking it, this should be fixed now (to be observed) 
+* Template CSS conflicts solved in the plugin
+* Facebook sharing text issues fixed
+
+= 1.0 =
+* First release
